4071751e5b1b77511e7d229788b84303aeba10e0
Matias Salinas <matixxdpls@gmail.com>
2022-09-14 05:29:21 -0300

    Added color picker, grid slider bar and clear button

--- javascript.js
+++ javascript.js
@@ -1,21 +1,40 @@
 let container = document.querySelector('.container');
+const slider = document.querySelector('.slider');
+const output = document.getElementById('value');
+const clear = document.getElementById('clear')
 
-let size = prompt("Choose gride size");
-let cuadros = size* size;
-document.getElementById('1').style.gridTemplateColumns = `repeat(${size},1fr)`;
+output.textContent = `${slider.value}x${slider.value} `;
+let size = slider.value;
 
-console.log(container.gridTemplateColumns)
 
-for(cuadros;cuadros>0;cuadros--){
-    let square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+clear.addEventListener('click', draw);
+
+//updates text and value of the slider
+slider.oninput =function(){
+    output.textContent = `${this.value}x${this.value} `;
+    size = slider.value;
+    
 }
 
-const squares = document.querySelectorAll('.square');
 
-squares.forEach(square => {
-    square.addEventListener('mouseover',() =>{
-        square.style.backgroundColor = 'black';
+function draw(){
+    container.replaceChildren();//deletes all divs inside container
+    document.getElementById('1').style.gridTemplateColumns = `repeat(${size},1fr)`; //set grid columns
+    let cuadros = size* size;
+    for(cuadros;cuadros>0;cuadros--){ //adds the divs to the grid
+        let square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+}
+mouseDetection();
+}
+
+function mouseDetection(){
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.addEventListener('mouseover',() =>{
+            square.style.backgroundColor = document.getElementById('cvalue').value;
+        })
     })
-})
+}
+draw();
