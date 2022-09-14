fc1f64f45d94f99ef348acc1a8afe50f087c1e46
Matias Salinas <matixxdpls@gmail.com>
2022-09-14 16:30:53 -0300

    rainbown mode added

--- javascript.js
+++ javascript.js
@@ -2,17 +2,32 @@ let container = document.querySelector('.container');
 const slider = document.querySelector('.slider');
 const output = document.getElementById('value');
 const clear = document.getElementById('clear')
+const rMode = document.getElementById('rainbow')
+
+// const darken = document.getElementById('shadow')
+// const lighten = document.getElementById('light')
 
 output.textContent = `${slider.value}x${slider.value} `;
 let size = slider.value;
-
+let rainbow = false;
+// let dark = false;
+// let light = false;
 
 clear.addEventListener('click', draw);
+
+rMode.addEventListener('click',() =>    {
+    rainbow = !rainbow;
+});//toggle rainbow mode on click
+
+// darken.addEventListener('click',() =>    {
+//     dark = !dark;
+// });
 
 //updates text and value of the slider
 slider.oninput =function(){
     output.textContent = `${this.value}x${this.value} `;
     size = slider.value;
+    draw();
     
 }
 
@@ -33,8 +48,13 @@ function mouseDetection(){
     const squares = document.querySelectorAll('.square');
     squares.forEach(square => {
         square.addEventListener('mouseover',() =>{
+            if(!rainbow){
             square.style.backgroundColor = document.getElementById('cvalue').value;
+        }else{
+            square.style.backgroundColor = "#" + ((1<<24)*Math.random() | 0).toString(16) // random color value generator
+        }
         })
     })
 }
+
 draw();
